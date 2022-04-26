400b03e5cd334040832ffaf049232ead958e376b
Elad Ohana <69722689+eohana-unc@users.noreply.github.com>
2022-04-25 23:19:31 -0400

    Updated form fields

--- src/App.js
+++ src/App.js
@@ -46,13 +46,13 @@ function App() {
     let currentA05 = document.querySelector('#a05').value
     let currentE01 = document.querySelector('#e01').value
     let currentE02 = document.querySelector('#e02').value
-    let currentmid = document.querySelector('#e01').value
-
+    let currentMidterm = document.querySelector('#midterm').value
+    let currentCommits = document.querySelector('#commits').value
 
     if(currentSum==='')
     return;
 // 2. This line is where you add the points all together. Add your other variable references here. Make sure that they match what is above and what is below in the HTML form
-    let sum = parseInt(currentA00) + parseInt(currentA01);
+    let sum = parseInt(currentA00) + parseInt(currentA01) + parseInt(currentA02) + parseInt(currentA03) + parseInt(currentA04) + parseInt(currentA05) + parseInt(currentE01) + parseInt(currentE02) + parseInt(currentCommits) + parseInt(currentMidterm);
     setCurrentSum(sum);
   }
 
@@ -69,6 +69,14 @@ function App() {
       <form>
             <input type="text" id="a00" placeholder="a00" /><br/><br/>
             <input type="text" id="a01" placeholder="a01" /><br/><br/>
+            <input type="text" id="a02" placeholder="a02" /><br/><br/>
+            <input type="text" id="a03" placeholder="a03" /><br/><br/>
+            <input type="text" id="a04" placeholder="a04" /><br/><br/>
+            <input type="text" id="a05" placeholder="a05" /><br/><br/>
+            <input type="text" id="e01" placeholder="e01" /><br/><br/>
+            <input type="text" id="e02" placeholder="e02" /><br/><br/>
+            <input type="text" id="midterm" placeholder="midterm" /><br/><br/>
+            <input type="text" id="commits" placeholder="commits" /><br/><br/>
             <button onClick={Add}>Add</button>
             <button onClick={Clear}>Clear</button>
             <label for="result"><b>Result:</b></label>
